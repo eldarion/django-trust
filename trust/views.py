14acf62c5f043e052e032ce92338d8eef221ab6b
Donald Stufft <donald.stufft@gmail.com>
2012-07-07 11:59:09 -0400

    Require staff access to view rating pages

--- trust/views.py
+++ trust/views.py
@@ -1,5 +1,8 @@
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView, UpdateView
+from django.utils.decorators import method_decorator
+
+from django.contrib.admin.views.decorators import staff_member_required
 
 from trust.models import TrustItem
 from trust.forms import RateForm
@@ -8,8 +11,16 @@ from trust.forms import RateForm
 class TrustQueue(ListView):
     queryset = TrustItem.objects.filter(rating=None)
 
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super(TrustQueue, self).dispatch(*args, **kwargs)
+
 
 class RateView(UpdateView):
     model = TrustItem
     form_class = RateForm
     success_url = reverse_lazy("manage_trust_queue")
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super(RateView, self).dispatch(*args, **kwargs)
